18ba211b2b5758e7f0737ecb38e320e2fd024218
Olivier Migliorini <olivier.migliorini@gmail.com>
2020-08-16 19:31:53 +0200

    CMake

--- src/Question2.h
+++ src/Question2.h
@@ -15,4 +15,4 @@ bool discard_elements_above(uint32_t T, const std::vector<uint32_t>& I, std::vec
 /* @return : true if T is even and T/2 has been found twice, false otherwise*/
 bool split_elements(uint32_t T, const std::vector<uint32_t>& elements, std::vector<uint32_t>& elementsBelowTOver2, std::vector<uint32_t>& elementsAboveTOver2);
 
-void find_largest_sum(uint32_t T, const std::vector<uint32_t>& I, vector<uint32_t>& M, uint32_t& S);
+void find_largest_sum(uint32_t T, const std::vector<uint32_t>& I, std::vector<uint32_t>& M, uint32_t& S);
