6b5a3395a10a33199a2a7a096b19ad772d996639
Olivier Migliorini <olivier.migliorini@gmail.com>
2020-08-16 18:32:37 +0200

    Unit Test

---
+++ tst/Question1-test.cpp
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+#include "Question1.h"
+
+TEST(addTest, test) 
+{
+    EXPECT_EQ (add(0,0), 0);
+    EXPECT_EQ (add(12,10), 0);
+    EXPECT_EQ (add(-13,16), 0);
+    EXPECT_EQ (add(20,-24), 0);
+    EXPECT_EQ (add(-26,-14), 0);
+    EXPECT_EQ (add(INT_MAX, 1), 0);
+}
---
+++ tst/Question2-test.cpp
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+#include "Question2.h"
+
+TEST(find_largest_sumTest, test) 
+{
+}
